edd2933e0f15b00e3357758b38e582b9dae07f0a
Ploppa-Smurph <jeffreywalley@student.bpcc.edu>
2024-05-09 21:45:09 -0500

    work on resumesite

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,12 @@
 import styles from './App.module.css';
-import { navbar } from "./components/navbar/navbar";
+import { Navbar } from "./components/navbar/navbar";
 
 function App() {
-  return <div className={styles.App}>
-    <navbar />
-  </div>;
+  return (
+  <div className={styles.App}>
+    <Navbar />
+  </div>
+  );
 }
 
 export default App;
--- src/components/navbar/navbar.jsx
+++ src/components/navbar/navbar.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
 
-export const navbar = () => {
-  return <div>navbar</div>;
+export const Navbar = () => {
+  return <div>Navbar</div>;
 };
