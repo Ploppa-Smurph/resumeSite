aa379df13b37dfda106926c51424f6ad0e03cd80
Ploppa-Smurph <jeffreywalley@student.bpcc.edu>
2024-05-09 21:14:40 -0500

    work on resumeSite

---
+++ src/utils.js
@@ -0,0 +1,6 @@
+// takes relative path and constructs absolute path 
+// makes easier to share images in modules
+
+export const getImageURL = (path) => {
+    return new URL(`assets/${path}`, import.meta.url).href;
+}
