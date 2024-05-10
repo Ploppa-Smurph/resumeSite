8decf9c620f4eb879c3aaaa72cd4634ad05181ae
Ploppa-Smurph <jeffreywalley@student.bpcc.edu>
2024-05-10 17:55:06 -0500

    work on resume project

---
+++ src/components/Contact/Contact.jsx
@@ -0,0 +1,29 @@
+import React from 'react';
+import styles from "./Contact.module.css";
+import {getImageURL} from "../../utils.js";
+
+export const Contact = () => {
+  return (
+    <footer id="contact" className={styles.container}>
+        <div className={styles.text}>
+            <h2>Contact</h2>
+            <p>Initiate Contact!</p>
+        </div>
+        <ul className={styles.links}>
+            <li className={styles.link}>
+                <img src={getImageURL("contact/email.png")} alt="Email icon" />
+                <a href="mailto:jswalley@rocketmail.com">jswalley@rocketmail.com</a>
+            </li>
+            <li className={styles.link}>
+                <img src={getImageURL("contact/linkedin.png")} alt="Linkedin icon" />
+                <a href="https://www.linkedin.com/in/j-s-walley/">Linkedin</a>
+            </li>
+            <li className={styles.link}>
+                <img src={getImageURL("contact/github.png")} alt="Github icon" />
+                <a href="https://github.com/Ploppa-Smurph">Github</a>
+            </li>
+        </ul>
+    </footer>
+
+  );
+};
