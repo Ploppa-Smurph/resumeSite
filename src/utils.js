0a589373894934533aeb0b595356f926943bb5ac
Ploppa-Smurph <jeffreywalley@student.bpcc.edu>
2024-05-17 15:33:55 -0500

    updated resume site

--- src/utils.js
+++ src/utils.js
@@ -1,5 +1,5 @@
 // takes relative path and constructs absolute path 
-// makes easier to share images in modules
+// makes easier to share images between modules
 
 export const getImageURL = (path) => {
     return new URL(`/assets/${path}`, import.meta.url).href;
